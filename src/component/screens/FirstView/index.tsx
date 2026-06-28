983236b1efbb78c3a0ec525961e2bcafc429922d
orotigen <tomo2708@outlook.jp>
2026-06-29 08:20:57 +0900

    firseView進めてる

--- src/component/screens/FirstView/index.tsx
+++ src/component/screens/FirstView/index.tsx
@@ -2,8 +2,16 @@ import styles from "./style.module.css";
 
 function FirstView(){
     return (
-        <div>
+        <div className={styles.container}>
          <img src="/paeria_top.jpeg" alt="Paeria Top"/> 
+         <div className={styles['color-overlay']}></div>
+         <div className={styles['info-holder']}>
+            <h1>
+                The<br/>
+                Spanish<br/>
+                Heaven
+            </h1>
+         </div>
         </div>
     )
 }
